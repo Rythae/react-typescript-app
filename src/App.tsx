4819ae5204b575533beb7b37652cff7d8a9a5ded
Rythae <rythae@Rythaes-MacBook-Pro.local>
2023-02-21 14:08:04 +0100

    changes to files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
+import { Todo } from "./models/models";
 
-function App() {
+
+// let name: string;
+// let name: unknown;
+// let age: number;
+// let age: number | string;
+// let isStudent: boolean;
+// let hobbies: string[];
+// let role: [number, string]; //tuple
+
+// type Person = {
+//   name: string;
+//   age?: number; //question mark means age is optional you can choose to add or not add it
+// }
+
+// let person: Person = {
+//   name: "Smith"
+// }
+
+// let lotsOfPeople:  Person[];
+// let printName: Function;
+// function printName(name: string) {
+//   console.log(name)
+// }
+
+// interface Person  {
+//   name: string;
+//   age?: number; 
+// } using an interface instead of type keyword
+
+
+// let printName: (name: string) => void; //define a function that takes in a parameter name and returns nothing (void), it could as well return a number, string etc
+
+
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
-}
+};
 
 export default App;
